6494e305fed9557ecc9bbec01d0025826ddb1b4e
mlutzkan <mlutzkan@gmail.com>
2025-02-17 14:58:23 +0200

    a

--- main.tf
+++ main.tf
@@ -22,18 +22,6 @@ variable "source_directory" {
   default     = "quizz-app"
 }
 
-variable "access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
-
 
 provider "aws" {
   access_key = var.aws_access_key_id
