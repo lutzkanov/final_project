dd205a5998af7ac6bab56cdbd1f46cf0c10e98c4
mlutzkan <mlutzkan@gmail.com>
2025-02-17 15:43:34 +0200

    changed s3 bucket name

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "iambatman234" 
+  bucket = "iambatman1234" 
   acl    = "private"  
 
   website {
@@ -86,7 +86,7 @@ resource "aws_s3_bucket_policy" "website_bucket_policy" {
         Action    = "s3:GetObject"
         Effect    = "Allow"
         Principal = "*"
-        Resource  = "arn:aws:s3:::iambatman234/*"  
+        Resource  = "arn:aws:s3:::iambatman1234/*"  
       },
     ]
   })
